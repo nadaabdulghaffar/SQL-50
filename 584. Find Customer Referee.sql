411b090d2b7fd8010d87f5da23b01122fe9d49fc
Nada <nadaabdulghaffar42@gmail.com>
2024-11-24 18:05:57 +0200

    Two problems

---
+++ 584. Find Customer Referee.sql
@@ -0,0 +1,2 @@
+SELECT Customer.name FROM Customer
+Where NOT referee_id = 2 or referee_id is NULL
---
+++ 595-Big-Countries.sql
@@ -0,0 +1,4 @@
+SELECT name, population, area
+From World
+Where area >= 3000000 
+OR population >= 25000000
